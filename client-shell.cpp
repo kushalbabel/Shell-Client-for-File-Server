3b02ce871b6d781a4a1daae1bf4c8d16546c8f4a
Kushal Babel <babel97kushal@gmail.com>
2016-08-15 10:43:47 +0530

    unregistered SIGINT, shell prompt structure and cd done

--- client-shell.cpp
+++ client-shell.cpp
@@ -11,13 +11,14 @@
 using namespace std;
 
 int bytesReceived;
-
+string server_ip = "";
+string server_port  = "";
 	// SIGINT handler funciton
 void sigint_handler(int signo)
 {
 	cout << "\n\n";
 	cout << "Received SIGINT; downloaded " << bytesReceived  << " bytes so far." << endl;
-	exit(0);
+	return;
 }
 vector<string> tokenize(string command){
 	command+=" ";
@@ -49,14 +50,56 @@ void printTokens(vector<string> tokens){
 	}
 	cout<<endl;
 }
-	// main function
+void commandError(){
+	cout<<"Wrong command format"<<endl;
+	return;
+}
+int classifyCmd(vector<string> tokens){
+	if (tokens.size()==0) return -1;
+	if(tokens[0] == "cd"){
+		if(tokens.size() != 2){
+			return 0;
+		}
+		else return 1;
+	}
+	if(tokens[0] == "server"){
+		if(tokens.size()!= 3){
+			return 0;
+		}
+		else return 2;
+	}
+}
+void cdCmd(vector<string> tokens){
+	//implement cd
+	int status = chdir((tokens[1]).c_str());
+	if (status == 0){
+		//successful
+		cout<< "Directory changed to "<<tokens[1]<<endl;
+	}
+	else{
+		//error
+		if (status == -1){
+			cout<<"No such directory"<<endl;
+		}
+		else{
+			cout<<"error "<<status << " enccountered"<<endl;	
+		}		
+	}
+	return;
+}
+void serverCmd(vector <string> tokens){
+		server_ip = tokens[1];
+		server_port = tokens[2];
+		return;
+}
+// main function
 int main(int argc , char *argv[])
 {	
 	string command;
 	vector<string> tokens;
 	// register the sigint signal handler
-	if(signal(SIGINT, sigint_handler) == SIG_ERR)
-		cout << "Can't catch SIGINT" << endl;
+	// if(signal(SIGINT, sigint_handler) == SIG_ERR)
+	// 	cout << "Can't catch SIGINT" << endl;
 	while(true){
 		//prompt
 		cout<<"Hello> ";
@@ -64,7 +107,23 @@ int main(int argc , char *argv[])
 		getline(cin,command);
 		//tokenize
 		tokens = tokenize(command);
-		printTokens(tokens);
+		// printTokens(tokens);
+		int type = classifyCmd(tokens);
+		switch(type){
+			case -1:
+				continue;
+			case 0 :
+				commandError();
+				continue;
+			case 1:
+				cdCmd(tokens);
+				break;
+			case 2:
+				serverCmd(tokens);
+				break;
+			default :
+				commandError();
+		}
 	}
 	
 	return 0;
